403ffb8830e9f6909c91e70dfe3a9d7aa8683e49
Jack Bailey <jackbailey@jacks-mbp.mynet>
2020-02-12 19:28:00 +0000

    Updated rlikert() to account with bug where n = 1

--- R/rlikert.R
+++ R/rlikert.R
@@ -21,8 +21,8 @@ rlikert <- function(n = 100,
   # Warnings in case things go wrong
   if(length < 2){
     stop("length must be greater than or equal to 2")
-  } else if(n <= 0){
-    stop("n must be greater than 0")
+  } else if(n <= 1){
+    stop("n must be greater than 1")
   } else if(is.null(sd) == F) {
     if(sd <= 0){
       stop("sd must be greater than 0")
