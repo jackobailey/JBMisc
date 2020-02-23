95bf76d949f2ba71d930088aa0ddd8f1b8c9643d
Jack Bailey <jackbailey@jacks-mbp.mynet>
2020-02-23 19:25:54 +0000

    Move from absolute to relative font sizes.

--- R/theme_bailey.R
+++ R/theme_bailey.R
@@ -2,19 +2,20 @@
 #'
 #' This is a convenience function that makes it easier for me to make nice plots. It is an extension of theme_minimal() in ggplot2. It removes some lines, adds others, and changes the font to Cabin.
 #' @export
+#'
 
 theme_bailey <- function(){
   theme_minimal() +
     theme(text = element_text(family = "Cabin", color = "black", size = 8),
-          plot.title = element_text(family = "Cabin", face = "bold", size = 10, hjust = 0),
-          plot.subtitle = element_text(family = "Cabin", size = 8, hjust = 0),
+          plot.title = element_text(family = "Cabin", face = "bold", size = rel(1.5), hjust = 0),
+          plot.subtitle = element_text(family = "Cabin", size = rel(1.2), hjust = 0),
           axis.line = element_line(),
-          axis.title.x = element_text(family = "Cabin", face = "bold", margin = margin(t = 8), size = 6),
-          axis.text.x = element_text(color = "black", size = 6),
+          axis.title.x = element_text(family = "Cabin", face = "bold", margin = margin(t = 10), size = rel(1)),
+          axis.text.x = element_text(color = "black", size = rel(1)),
           axis.ticks.x = element_line(),
-          axis.title.y = element_text(family = "Cabin", face = "bold", margin = margin(r = 8), size = 6),
-          axis.text.y = element_text(color = "black", size = 6),
-          strip.text = element_text(family = "Cabin", face = "bold", size = 8),
+          axis.title.y = element_text(family = "Cabin", face = "bold", size = rel(1)),
+          axis.text.y = element_text(color = "black", size = rel(1)),
+          strip.text = element_text(family = "Cabin", face = "bold", size = rel(1.2)),
           panel.spacing = unit(.3, "cm"),
           panel.grid.major.y = element_line(size = .5),
           panel.grid.minor.y = element_blank(),
