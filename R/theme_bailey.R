929024f9f5c7a63a6cebfa0ce64da41c8599088f
Jack Bailey <jackbailey@jacks-mbp.mynet>
2020-08-27 18:34:08 +0100

    Revert to previous version of theme_bailey().

--- R/theme_bailey.R
+++ R/theme_bailey.R
@@ -4,44 +4,23 @@
 #' @export
 #'
 
-theme_bailey <- function(rmd = T){
-  if(rmd == T){
-    theme_minimal() +
-      theme(legend.title = element_text(face = "bold", size = rel(1)),
-            text = element_text(color = "black", size = 8),
-            plot.title = element_text(face = "bold", size = rel(1.4), hjust = 0),
-            plot.subtitle = element_text(size = rel(1), hjust = 0, margin = margin(b = 10)),
-            axis.line = element_line(lineend = "round"),
-            axis.title.x = element_text(face = "bold", margin = margin(t = 10), size = rel(1)),
-            axis.text.x = element_text(color = "black", size = rel(1)),
-            axis.ticks.x = element_line(lineend = "round"),
-            axis.title.y = element_text(face = "bold", size = rel(1)),
-            axis.text.y = element_text(color = "black", size = rel(1)),
-            strip.text = element_text(face = "bold", size = rel(1)),
-            panel.spacing = unit(.3, "cm"),
-            panel.grid.major.y = element_line(size = .5, lineend = "round"),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.grid.minor.x = element_blank()
-      )
-  } else {
-    theme_minimal() +
-      theme(legend.title = element_text(family = "Cabin", face = "bold", size = rel(1)),
-            text = element_text(family = "Cabin", color = "black", size = 8),
-            plot.title = element_text(family = "Cabin", face = "bold", size = rel(1.4), hjust = 0),
-            plot.subtitle = element_text(family = "Cabin", size = rel(1), hjust = 0, margin = margin(b = 10)),
-            axis.line = element_line(lineend = "round"),
-            axis.title.x = element_text(family = "Cabin", face = "bold", margin = margin(t = 10), size = rel(1)),
-            axis.text.x = element_text(color = "black", size = rel(1)),
-            axis.ticks.x = element_line(lineend = "round"),
-            axis.title.y = element_text(family = "Cabin", face = "bold", size = rel(1)),
-            axis.text.y = element_text(color = "black", size = rel(1)),
-            strip.text = element_text(family = "Cabin", face = "bold", size = rel(1)),
-            panel.spacing = unit(.3, "cm"),
-            panel.grid.major.y = element_line(size = .5, lineend = "round"),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.grid.minor.x = element_blank()
-      )
-  }
+theme_bailey <- function(){
+  theme_minimal() +
+    theme(legend.title = element_text(family = "Cabin", face = "bold", size = rel(1)),
+          text = element_text(family = "Cabin", color = "black", size = 8),
+          plot.title = element_text(family = "Cabin", face = "bold", size = rel(1.4), hjust = 0),
+          plot.subtitle = element_text(family = "Cabin", size = rel(1), hjust = 0, margin = margin(b = 10)),
+          axis.line = element_line(lineend = "round"),
+          axis.title.x = element_text(family = "Cabin", face = "bold", margin = margin(t = 10), size = rel(1)),
+          axis.text.x = element_text(color = "black", size = rel(1)),
+          axis.ticks.x = element_line(lineend = "round"),
+          axis.title.y = element_text(family = "Cabin", face = "bold", size = rel(1)),
+          axis.text.y = element_text(color = "black", size = rel(1)),
+          strip.text = element_text(family = "Cabin", face = "bold", size = rel(1)),
+          panel.spacing = unit(.3, "cm"),
+          panel.grid.major.y = element_line(size = .5, lineend = "round"),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank()
+    )
 }
